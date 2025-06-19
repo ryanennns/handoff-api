ca2dc60d964a76fb2215d32c91601d35fb9a4608
Ryan Enns <ryanennns@gmail.com>
2025-06-19 09:03:45 -0400

    dummy

--- routes/api.php
+++ routes/api.php
@@ -22,15 +22,15 @@ Route::prefix('auth')->name('auth.')->group(function () {
         });
 
         Route::get('callback', function () {
-            $githubUser = Socialite::driver('github')->user();
+            $googleUser = Socialite::driver('google')->user();
 
             $user = User::updateOrCreate([
-                'github_id' => $githubUser->id,
+                'google_id' => $googleUser->id,
             ], [
-                'name'                 => $githubUser->name,
-                'email'                => $githubUser->email,
-                'github_token'         => $githubUser->token,
-                'github_refresh_token' => $githubUser->refreshToken,
+                'name'                 => $googleUser->name,
+                'email'                => $googleUser->email,
+                'google_token'         => $googleUser->token,
+                'google_refresh_token' => $googleUser->refreshToken,
             ]);
 
             Auth::login($user);
