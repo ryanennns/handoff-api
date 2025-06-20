e1ed51843159d4d5240faa862a96c611cfe677ee
Ryan Enns <ryanennns@gmail.com>
2025-06-20 14:10:25 -0400

    kekw

--- app/Models/PlaylistTransfer.php
+++ app/Models/PlaylistTransfer.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class PlaylistTransfer extends Model
 {
-    //
+    use HasUuids;
+
+    protected $guarded = [];
 }
