1123708237c59eac94c85a14ed22df7c8dcdb5e7
Ryan Enns <ryanennns@gmail.com>
2025-06-19 21:11:04 -0400

    init spotify oauth in AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,22 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
+use SocialiteProviders\Manager\SocialiteWasCalled;
+use SocialiteProviders\Spotify\Provider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        Event::listen(function (SocialiteWasCalled $event) {
+            $event->extendSocialite('spotify', Provider::class);
+        });
     }
 }
