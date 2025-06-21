74b8d30b77414db2a3aecc6891899fe5c9633990
Ryan Enns <ryanennns@gmail.com>
2025-06-21 19:43:50 -0400

    ironing out tidal API client

--- app/Services/TidalApi.php
+++ app/Services/TidalApi.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Helpers\Track;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
@@ -13,26 +14,37 @@ class TidalApi extends StreamingServiceApi
 
     public function getPlaylists(): array
     {
-        $response = Http::withToken($this->oauthCredential->token)
+        $playlistResponse = Http::withToken($this->oauthCredential->token)
             ->get(self::BASE_URL . '/playlists', [
                 'countryCode'         => 'CA',
                 'filter[r.owners.id]' => $this->oauthCredential->provider_id,
             ]);
 
-        $json = $response->json();
-        Log::info('tidal api response', $json);
+        $json = $playlistResponse->json();
+
 
         return collect(Arr::get($json, 'data', []))
-            ->map(fn($item) => [
-                'id'               => Arr::get($item, 'id'),
-                'name'             => Arr::get($item, 'attributes.name'),
-                'tracks'           => null,
-                'owner'            => [
-                    'display_name' => Arr::get($item, 'attributes.relationships.owners.data', ''),
-                ],
-                'number_of_tracks' => Arr::get($item, 'attributes.numberOfItems'),
-                'image_uri'        => Arr::get($item, 'attributes.relatinoships.coverArt.data', ''),
-            ])->toArray();
+            ->map(function ($item) {
+                $tracksUrl = Arr::get($item, 'relationships.items.links.self');
+
+                $tracksResponse = Http::withToken($this->oauthCredential->token)
+                    ->get(self::BASE_URL . '/' . $tracksUrl);
+
+                $tracksJson = $tracksResponse->json();
+                $tracks = collect(Arr::get($tracksJson, 'data', []))
+                    ->map(fn($track) => ['id' => Arr::get($track, 'id')]);
+
+                return [
+                    'id'               => Arr::get($item, 'id'),
+                    'name'             => Arr::get($item, 'attributes.name'),
+                    'tracks'           => $tracks,
+                    'owner'            => [
+                        'display_name' => Arr::get($item, 'attributes.relationships.owners.data', ''),
+                    ],
+                    'number_of_tracks' => Arr::get($item, 'attributes.numberOfItems'),
+                    'image_uri'        => null,
+                ];
+            })->toArray();
     }
 
     public function getPlaylistTracks(string $playlistId): array
