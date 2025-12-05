66d159e7af252e62f2f3be8b600733c78012c5fa
ryan <ryanennns@gmail.com>
2025-12-05 14:56:31 -0500

    extract payload to SpotifyResponse

---
+++ tests/SpotifyResponse.php
@@ -0,0 +1,298 @@
+<?php
+
+namespace Tests;
+
+class SpotifyResponse
+{
+    public const PLAYLIST_TRACKS_RESPONSE_ITEMS = [
+        [
+            "added_at"        => "2023-08-12T19:14:59Z",
+            "added_by"        => [
+                "external_urls" => [
+                    "spotify" => "https://open.spotify.com/user/pxd0l8njl07f1wlvhf09zo01k"
+                ],
+                "href"          => "https://api.spotify.com/v1/users/pxd0l8njl07f1wlvhf09zo01k",
+                "id"            => "pxd0l8njl07f1wlvhf09zo01k",
+                "type"          => "user",
+                "uri"           => "spotify:user:pxd0l8njl07f1wlvhf09zo01k"
+            ],
+            "is_local"        => false,
+            "primary_color"   => null,
+            "track"           => [
+                "preview_url"       => null,
+                "available_markets" => [],
+                "explicit"          => false,
+                "type"              => "track",
+                "episode"           => false,
+                "track"             => true,
+                "album"             => [
+                    "available_markets"      => [],
+                    "type"                   => "album",
+                    "album_type"             => "single",
+                    "href"                   => "https://api.spotify.com/v1/albums/0FawTVuFbmS9KLWA8TAtau",
+                    "id"                     => "0FawTVuFbmS9KLWA8TAtau",
+                    "images"                 => [
+                        [
+                            "height" => 640,
+                            "url"    => "https://i.scdn.co/image/ab67616d0000b2736758b0ff4e8745ca7386921f",
+                            "width"  => 640
+                        ],
+                        [
+                            "height" => 300,
+                            "url"    => "https://i.scdn.co/image/ab67616d00001e026758b0ff4e8745ca7386921f",
+                            "width"  => 300
+                        ],
+                        [
+                            "height" => 64,
+                            "url"    => "https://i.scdn.co/image/ab67616d000048516758b0ff4e8745ca7386921f",
+                            "width"  => 64
+                        ]
+                    ],
+                    "name"                   => "Six Forty Seven",
+                    "release_date"           => "2016-03-28",
+                    "release_date_precision" => "day",
+                    "uri"                    => "spotify:album:0FawTVuFbmS9KLWA8TAtau",
+                    "artists"                => [
+                        [
+                            "external_urls" => [
+                                "spotify" => "https://open.spotify.com/artist/3ctnkEZGtVBTxS7IMin8nC"
+                            ],
+                            "href"          => "https://api.spotify.com/v1/artists/3ctnkEZGtVBTxS7IMin8nC",
+                            "id"            => "3ctnkEZGtVBTxS7IMin8nC",
+                            "name"          => "Instupendo",
+                            "type"          => "artist",
+                            "uri"           => "spotify:artist:3ctnkEZGtVBTxS7IMin8nC"
+                        ]
+                    ],
+                    "external_urls"          => [
+                        "spotify" => "https://open.spotify.com/album/0FawTVuFbmS9KLWA8TAtau"
+                    ],
+                    "total_tracks"           => 1
+                ],
+                "artists"           => [
+                    [
+                        "external_urls" => [
+                            "spotify" => "https://open.spotify.com/artist/3ctnkEZGtVBTxS7IMin8nC"
+                        ],
+                        "href"          => "https://api.spotify.com/v1/artists/3ctnkEZGtVBTxS7IMin8nC",
+                        "id"            => "3ctnkEZGtVBTxS7IMin8nC",
+                        "name"          => "Instupendo",
+                        "type"          => "artist",
+                        "uri"           => "spotify:artist:3ctnkEZGtVBTxS7IMin8nC"
+                    ]
+                ],
+                "disc_number"       => 1,
+                "track_number"      => 1,
+                "duration_ms"       => 119990,
+                "external_ids"      => [
+                    "isrc" => "QM4DW1618349"
+                ],
+                "external_urls"     => [
+                    "spotify" => "https://open.spotify.com/track/2WuK1opHQzRo18YXCtY2DF"
+                ],
+                "href"              => "https://api.spotify.com/v1/tracks/2WuK1opHQzRo18YXCtY2DF",
+                "id"                => "2WuK1opHQzRo18YXCtY2DF",
+                "name"              => "Six Forty Seven",
+                "popularity"        => 2,
+                "uri"               => "spotify:track:2WuK1opHQzRo18YXCtY2DF",
+                "is_local"          => false
+            ],
+            "video_thumbnail" => [
+                "url" => null
+            ]
+        ],
+        [
+            "added_at"        => "2023-08-12T19:15:02Z",
+            "added_by"        => [
+                "external_urls" => [
+                    "spotify" => "https://open.spotify.com/user/pxd0l8njl07f1wlvhf09zo01k"
+                ],
+                "href"          => "https://api.spotify.com/v1/users/pxd0l8njl07f1wlvhf09zo01k",
+                "id"            => "pxd0l8njl07f1wlvhf09zo01k",
+                "type"          => "user",
+                "uri"           => "spotify:user:pxd0l8njl07f1wlvhf09zo01k"
+            ],
+            "is_local"        => false,
+            "primary_color"   => null,
+            "track"           => [
+                "preview_url"       => null,
+                "explicit"          => false,
+                "type"              => "track",
+                "episode"           => false,
+                "track"             => true,
+                "album"             => [
+                    "type"                   => "album",
+                    "album_type"             => "single",
+                    "href"                   => "https://api.spotify.com/v1/albums/01qVg4PWWaeremZOHDVRS1",
+                    "id"                     => "01qVg4PWWaeremZOHDVRS1",
+                    "images"                 => [
+                        [
+                            "height" => 640,
+                            "url"    => "https://i.scdn.co/image/ab67616d0000b2734a41ed550aa0d6566e43b3f2",
+                            "width"  => 640
+                        ],
+                        [
+                            "height" => 300,
+                            "url"    => "https://i.scdn.co/image/ab67616d00001e024a41ed550aa0d6566e43b3f2",
+                            "width"  => 300
+                        ],
+                        [
+                            "height" => 64,
+                            "url"    => "https://i.scdn.co/image/ab67616d000048514a41ed550aa0d6566e43b3f2",
+                            "width"  => 64
+                        ]
+                    ],
+                    "name"                   => "Comfort Chain",
+                    "release_date"           => "2017-01-20",
+                    "release_date_precision" => "day",
+                    "uri"                    => "spotify:album:01qVg4PWWaeremZOHDVRS1",
+                    "artists"                => [
+                        [
+                            "external_urls" => [
+                                "spotify" => "https://open.spotify.com/artist/3ctnkEZGtVBTxS7IMin8nC"
+                            ],
+                            "href"          => "https://api.spotify.com/v1/artists/3ctnkEZGtVBTxS7IMin8nC",
+                            "id"            => "3ctnkEZGtVBTxS7IMin8nC",
+                            "name"          => "Instupendo",
+                            "type"          => "artist",
+                            "uri"           => "spotify:artist:3ctnkEZGtVBTxS7IMin8nC"
+                        ]
+                    ],
+                    "external_urls"          => [
+                        "spotify" => "https://open.spotify.com/album/01qVg4PWWaeremZOHDVRS1"
+                    ],
+                    "total_tracks"           => 1
+                ],
+                "artists"           => [
+                    [
+                        "external_urls" => [
+                            "spotify" => "https://open.spotify.com/artist/3ctnkEZGtVBTxS7IMin8nC"
+                        ],
+                        "href"          => "https://api.spotify.com/v1/artists/3ctnkEZGtVBTxS7IMin8nC",
+                        "id"            => "3ctnkEZGtVBTxS7IMin8nC",
+                        "name"          => "Instupendo",
+                        "type"          => "artist",
+                        "uri"           => "spotify:artist:3ctnkEZGtVBTxS7IMin8nC"
+                    ]
+                ],
+                "disc_number"       => 1,
+                "track_number"      => 1,
+                "duration_ms"       => 184363,
+                "external_ids"      => [
+                    "isrc" => "QZ5D71600005"
+                ],
+                "external_urls"     => [
+                    "spotify" => "https://open.spotify.com/track/0Db8f7uikjXxH3qHe0UKoW"
+                ],
+                "href"              => "https://api.spotify.com/v1/tracks/0Db8f7uikjXxH3qHe0UKoW",
+                "id"                => "0Db8f7uikjXxH3qHe0UKoW",
+                "name"              => "Comfort Chain",
+                "popularity"        => 73,
+                "uri"               => "spotify:track:0Db8f7uikjXxH3qHe0UKoW",
+                "is_local"          => false
+            ],
+            "video_thumbnail" => [
+                "url" => null
+            ]
+        ],
+        [
+            "added_at"        => "2023-08-12T19:15:04Z",
+            "added_by"        => [
+                "external_urls" => [
+                    "spotify" => "https://open.spotify.com/user/pxd0l8njl07f1wlvhf09zo01k"
+                ],
+                "href"          => "https://api.spotify.com/v1/users/pxd0l8njl07f1wlvhf09zo01k",
+                "id"            => "pxd0l8njl07f1wlvhf09zo01k",
+                "type"          => "user",
+                "uri"           => "spotify:user:pxd0l8njl07f1wlvhf09zo01k"
+            ],
+            "is_local"        => false,
+            "primary_color"   => null,
+            "track"           => [
+                "preview_url"       => null,
+                "explicit"          => false,
+                "type"              => "track",
+                "episode"           => false,
+                "track"             => true,
+                "album"             => [
+                    "type"                   => "album",
+                    "album_type"             => "single",
+                    "href"                   => "https://api.spotify.com/v1/albums/3PQQmgHwmrt6Eta9eI8zjt",
+                    "id"                     => "3PQQmgHwmrt6Eta9eI8zjt",
+                    "images"                 => [
+                        [
+                            "height" => 640,
+                            "url"    => "https://i.scdn.co/image/ab67616d0000b2731bdf6d45aaa8a7cee6c88c9c",
+                            "width"  => 640
+                        ],
+                        [
+                            "height" => 300,
+                            "url"    => "https://i.scdn.co/image/ab67616d00001e021bdf6d45aaa8a7cee6c88c9c",
+                            "width"  => 300
+                        ],
+                        [
+                            "height" => 64,
+                            "url"    => "https://i.scdn.co/image/ab67616d000048511bdf6d45aaa8a7cee6c88c9c",
+                            "width"  => 64
+                        ]
+                    ],
+                    "name"                   => "Falling",
+                    "release_date"           => "2016-09-16",
+                    "release_date_precision" => "day",
+                    "uri"                    => "spotify:album:3PQQmgHwmrt6Eta9eI8zjt",
+                    "artists"                => [
+                        [
+                            "external_urls" => [
+                                "spotify" => "https://open.spotify.com/artist/3ctnkEZGtVBTxS7IMin8nC"
+                            ],
+                            "href"          => "https://api.spotify.com/v1/artists/3ctnkEZGtVBTxS7IMin8nC",
+                            "id"            => "3ctnkEZGtVBTxS7IMin8nC",
+                            "name"          => "Instupendo",
+                            "type"          => "artist",
+                            "uri"           => "spotify:artist:3ctnkEZGtVBTxS7IMin8nC"
+                        ]
+                    ],
+                    "external_urls"          => [
+                        "spotify" => "https://open.spotify.com/album/3PQQmgHwmrt6Eta9eI8zjt"
+                    ],
+                    "total_tracks"           => 1
+                ],
+                "artists"           => [
+                    [
+                        "external_urls" => [
+                            "spotify" => "https://open.spotify.com/artist/3ctnkEZGtVBTxS7IMin8nC"
+                        ],
+                        "href"          => "https://api.spotify.com/v1/artists/3ctnkEZGtVBTxS7IMin8nC",
+                        "id"            => "3ctnkEZGtVBTxS7IMin8nC",
+                        "name"          => "Instupendo",
+                        "type"          => "artist",
+                        "uri"           => "spotify:artist:3ctnkEZGtVBTxS7IMin8nC"
+                    ]
+                ],
+                "disc_number"       => 1,
+                "track_number"      => 1,
+                "duration_ms"       => 119744,
+                "external_ids"      => [
+                    "isrc" => "UST8K1628643"
+                ],
+                "external_urls"     => [
+                    "spotify" => "https://open.spotify.com/track/5tG9KCkEnWc75cREtb4ffB"
+                ],
+                "href"              => "https://api.spotify.com/v1/tracks/5tG9KCkEnWc75cREtb4ffB",
+                "id"                => "5tG9KCkEnWc75cREtb4ffB",
+                "name"              => "Falling",
+                "popularity"        => 60,
+                "uri"               => "spotify:track:5tG9KCkEnWc75cREtb4ffB",
+                "is_local"          => false
+            ],
+            "video_thumbnail" => [
+                "url" => null
+            ]
+        ],
+    ];
+    public const PLAYLIST_TRACKS_RESPONSE_PAYLOAD = [
+        'items' => [
+            ...self::PLAYLIST_TRACKS_RESPONSE_ITEMS
+        ]
+    ];
+}
