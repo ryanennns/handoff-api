84a0e760fdb56ec4e791eac62c0ebfd2c42b8a3c
ryan <ryanennns@gmail.com>
2025-12-04 19:07:29 -0500

    seeder & readme

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,54 @@
 
 namespace Database\Seeders;
 
+use App\Models\PlaylistTransfer;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $user = User::factory()->create([
+            'name'  => 'Test User',
+            'email' => 'test@gmail.com',
         ]);
+
+        $user->playlistTransfers()
+            ->create(
+                PlaylistTransfer::factory()->raw([
+                    'source'      => 'spotify',
+                    'destination' => 'tidal',
+                    'status'      => 'completed'
+                ])
+            );
+
+        $user->playlistTransfers()
+            ->create(
+                PlaylistTransfer::factory()->raw([
+                    'source'      => 'spotify',
+                    'destination' => 'tidal',
+                    'status'      => 'in_progress'
+                ])
+            );
+
+        $user->playlistTransfers()
+            ->create(
+                PlaylistTransfer::factory()->raw([
+                    'source'      => 'spotify',
+                    'destination' => 'tidal',
+                    'status'      => 'failed'
+                ])
+            );
+
+        $user->playlistTransfers()
+            ->create(
+                PlaylistTransfer::factory()->raw([
+                    'source'      => 'spotify',
+                    'destination' => 'tidal',
+                    'status'      => 'pending'
+                ])
+            );
     }
 }
