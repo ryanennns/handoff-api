83841cc20916ad55c42bd64df1a8bd2169cc0184
Ryan Enns <ryanennns@gmail.com>
2025-12-10 14:35:49 -0500

    minor refactor

--- app/Observers/PlaylistTransferObserver.php
+++ app/Observers/PlaylistTransferObserver.php
@@ -13,17 +13,15 @@ class PlaylistTransferObserver
             $playlistTransfer->isDirty('status')
             || $playlistTransfer->isDirty('playlists_processed')
         ) {
-            PlaylistTransferStatusUpdated::dispatch($playlistTransfer);
 
-            return;
-        }
+            if (
+                $playlistTransfer->playlists_processed === count($playlistTransfer->playlists)
+            ) {
+                // oh god no
+                $playlistTransfer->status = PlaylistTransfer::STATUS_COMPLETED;
+                $playlistTransfer->saveQuietly();
+            }
 
-        if (
-            $playlistTransfer->playlists_processed === count($playlistTransfer->playlists)
-        ) {
-            // oh god no
-            $playlistTransfer->status = PlaylistTransfer::STATUS_COMPLETED;
-            $playlistTransfer->saveQuietly();
             PlaylistTransferStatusUpdated::dispatch($playlistTransfer);
         }
     }
